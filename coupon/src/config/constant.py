ffb8292707db4a921c541fc6f95731e3a1089c26
PC <wangzhaojie219@163.com>
2026-01-03 10:41:00 +0800

    coupon 项目提出为一个独立的项目，不在scripts仓库里了

--- coupon/src/config/constant.py
+++
@@ -1,5 +0,0 @@
-import os
-
-home_path = os.path.join(os.path.expanduser("~"), "Desktop")
-backup_path = os.path.join(os.path.expanduser("~"), "Desktop", "backup")
-template_path = os.path.join(os.path.expanduser("~"), "Desktop", "template")
